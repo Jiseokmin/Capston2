004e0ea982068641f32662c38502fbc0f3608cf1
Jiseokmin <37jr@naver.com>
2018-11-08 22:43:39 +0900

    스플래쉬 Activity 와 사용자 정보를 담는 User 그밖에 로고 이미지 , 말풍선 이미지 등 여러 이미지

---
+++ app/src/main/java/kr/study/capston2/SplashActivity.java
@@ -0,0 +1,25 @@
+package kr.study.capston2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(1400);
+
+        }catch (InterruptedException E) {
+            E.printStackTrace();
+        }
+
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+
+    }
+}
---
+++ app/src/main/java/kr/study/capston2/User.java
@@ -0,0 +1,16 @@
+package kr.study.capston2;
+
+public class User {
+
+    public String point;
+
+    public User() {
+
+    }
+
+    public User(String point) {
+        this.point = point;
+
+    }
+
+}
